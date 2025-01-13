b3895f3894e6dc619a1b28db03d23477e60469b6
Nasmin <fathimanesmi@gmail.com>
2025-01-13 09:27:00 -0500

    weather main view complete

---
+++ WeatherApp/WeatherApp/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Fathima Nasmin on 1/13/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Gradients {
+	static let cloudy = LinearGradient(gradient: Gradient(colors: [.blue, .gray]), startPoint: .top, endPoint: .bottom)
+}
--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		GeometryReader { geo in
+			ZStack {
+				Gradients.cloudy
+				
+				VStack {
+					HStack(alignment: .bottom) {
+						// cloudy and temp
+						VStack {
+							HStack {
+								Image(systemName: "cloud")
+								Text("Cloudy")
+							}
+							HStack(alignment: .top) {
+								Text("15")
+									.font(.system(size: 96))
+								Image(systemName: "circle")
+									.padding(.top, 10)
+									.font(.system(size: 20))
+							}
+							.padding(.top, -5)
+						}
+						.padding()
+						.font(.largeTitle)
+						// low and high
+						VStack {
+							HStack {
+								Text("21")
+									.padding(.bottom, -1)
+								Image(systemName: "degreesign.celsius")
+									.font(.title)
+							}
+							Rectangle()
+								.frame(width: geo.size.width / 4, height: 1)
+							
+							HStack {
+								Text("15")
+									.padding(.top, -1)
+								Image(systemName: "degreesign.celsius")
+									.font(.title)
+							}
+						}
+						.padding()
+						.padding(.bottom, 10)
+						.font(.largeTitle)
+					}
+				}
+				.foregroundColor(.white)
+			}
+			.ignoresSafeArea()
+		}
+
     }
 }
 
