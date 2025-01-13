f59b6fa1becd6787b8d1c73bb976b4ae53330642
Nasmin <fathimanesmi@gmail.com>
2025-01-13 07:12:39 -0500

    Project setup

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Fathima Nasmin on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
