083da25a257cf1ddbde1b7c27efa9681ea7d519c
Nasmin <fathimanesmi@gmail.com>
2025-01-17 17:22:54 -0500

    Add an extension of Colors for hex code.

--- WeatherApp/WeatherApp/Utilities.swift
+++ WeatherApp/WeatherApp/Utilities.swift
@@ -6,10 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
 
 
 extension Double {
 	func rounded(to decimalPlace: Int) -> String {
 		String(format: "%.\(decimalPlace)f", self)
+	}
+}
+
+
+extension Color {
+	init(hex: String) {
+		let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+		let scanner = Scanner(string: hex)
+		
+		if hex.hasPrefix("#") {
+			scanner.currentIndex = hex.index(after: hex.startIndex)
+		}
+		
+		var rgbValue: UInt64 = 0
+		scanner.scanHexInt64(&rgbValue)
+		
+		let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+		let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+		let blue = Double(rgbValue & 0x0000FF) / 255.0
+		
+		self.init(.sRGB, red: red, green: green, blue: blue, opacity: 1.0)
 	}
 }
