ea0584ca786d8494245057dafce060ce01225733
Nasmin <fathimanesmi@gmail.com>
2025-01-21 15:24:29 -0500

    make changes to todo list

--- WeatherApp/WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -22,4 +22,5 @@ struct WeatherAppApp: App {
 // convert to C or F
 
 // @Action: Add to the city name to the core data or swift data
-
+// Core location: On app open, get the weather of the current location of the device.
+// core data or swift data: To store the several city names
