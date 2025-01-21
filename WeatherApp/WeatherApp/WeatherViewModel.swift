ce026443d6e81ebac241abba31a4997074a8c362
Nasmin <fathimanesmi@gmail.com>
2025-01-20 19:21:48 -0500

    Add new function for search feature to the viewmodel

--- WeatherApp/WeatherApp/WeatherViewModel.swift
+++ WeatherApp/WeatherApp/WeatherViewModel.swift
@@ -6,13 +6,35 @@
 //
 
 import Foundation
+import Combine
 
 class WeatherViewModel: ObservableObject {
 	@Published var forecast: WeatherModel
+	@Published var searchText: String = ""
+	@Published var cityList: [CityModel] = []
+	
+	private var cancellables: Set<AnyCancellable> = []
+	
+	private let fetcher = FetchService()
+	
+	var filteredCountry: [CityModel] {
+		guard !searchText.isEmpty && searchText.count >= 3 else { return [] }
+		
+		return cityList
+	}
+	
+	// Fetch the data based on searchText
+	@MainActor
+	func updateCityList() async {
+		do {
+			cityList = try await fetcher.fetchCitySuggestion(from: searchText)
+		}catch{
+			print("Error fetching: \(error)")
+		}
+	}
 	
 	init() {
 		let decoder = JSONDecoder()
-//		decoder.keyDecodingStrategy = .convertFromSnakeCase
 		decoder.dateDecodingStrategy = .secondsSince1970
 		
 		guard let data = Bundle.main.url(forResource: "sampleForecast", withExtension: "json") else {
@@ -26,5 +48,20 @@ class WeatherViewModel: ObservableObject {
 		}catch {
 			fatalError("Failed to load or decode JSON format : \(error)")
 		}
+		
+		// To observe searchText changes
+		$searchText
+			.debounce(for: .milliseconds(300), scheduler: DispatchQueue.main) // Prevents frequent API calls
+			.removeDuplicates() // avoid unnecessary updates
+			.sink { [weak self] searchText in
+				guard let self = self, searchText.count >= 3 else { return }
+				Task {
+					await self.updateCityList()
+				}
+			}
+			.store(in: &cancellables)
+		
 	}
+	
+	
 }
