f8544832df6ff94296b4722be935ace1558a833f
Nasmin <fathimanesmi@gmail.com>
2025-01-20 19:21:01 -0500

    add api into new files.

---
+++ WeatherApp/WeatherApp/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  WeatherApp
+//
+//  Created by Fathima Nasmin on 1/19/25.
+//
+
+import Foundation
+
+struct APIConstants {
+	
+	static let forecastAPIUrl = "https://api.weatherapi.com/v1/forecast.json?key=\(apiKey)"
+	static let searchAPIUrl = "https://api.weatherapi.com/v1/search.json"
+}
+
+
+//	static let searchAPIUrl = "http://api.weatherapi.com/v1/search.json?key=\(apiKey)"
